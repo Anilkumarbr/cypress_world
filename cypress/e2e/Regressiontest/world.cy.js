806cf8c43046d018652fa263d3e9c02fb97a61d0
anilkumarbr <banilkumar.in@gmail.com>
2025-01-03 13:04:59 +0530

    update

---
+++ cypress/e2e/Regressiontest/world.cy.js
@@ -0,0 +1,14 @@
+describe("alerts",()=>{
+    it("alert",()=>{
+    cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+    cy.contains("Click for JS Alert").click()
+    cy.on('window:alert',(text)=>{
+    
+        expect(text).contains("I am a JS Alert")
+        //added a comment
+        //hlo
+        //hlo the
+    
+    })
+})
+})
